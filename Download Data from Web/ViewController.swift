7a0191883a6c50cf1b76f6be99f7d5e83e89dbb9
asharijuang <asharijuang@me.com>
2015-08-20 06:20:10 +0700

    add url and task using nsurlsession 

--- Download Data from Web/ViewController.swift
+++ Download Data from Web/ViewController.swift
@@ -13,6 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.stackoverflow.com")!
+        
+        // nsurlsession seperti membuka browser, dan sharedsession seperti buka tab
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            // kondisi ketika selesai download data
+            if let urlContent = data {
+                // jika ada data maka jadikan sebagai urlContent
+                print(urlContent)
+            }else {
+                // jika ada error
+            }
+        }
+        
+        task.resume()
+        // Pada awalnya akan ada sedikit masalah dengan koneki yang menggunakan http, jadi kita perlu sedikit melakukan penambahan di info.plist
+        // NSAppTranportSecurity type dictionary
+        // NSAllowsArbitraryLoads boolean yes
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
